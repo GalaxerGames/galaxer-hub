dbf7506bb80b3ae8ae5b2d1573f9b76974014cfd
futjrnaut <futjrn@gmail.com>
2023-06-20 07:03:05 -0800

    import error

--- src/pages/ecosystem/glxr.tsx
+++ src/pages/ecosystem/glxr.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 import Link  from 'next/link';
+import Image from 'next/image';
 import styles from '../../components/modules/android.module.css';
 import '../../styles/globals.css';
 
