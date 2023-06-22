2d6f1504eddd3ac141fdf32cb6803ed9bf8a4aed
futjrnaut <futjrn@gmail.com>
2023-06-22 03:46:02 -0800

    claim page clean up passes multiplier

--- src/pages/claim.tsx
+++ src/pages/claim.tsx
@@ -1,35 +1,32 @@
 import { useState, useEffect } from 'react';
-import { InjectedConnector, connect, writeContract } from '@wagmi/core';
-import { createWalletClient, custom } from 'viem'; 
+import { createWalletClient, custom } from 'viem';
 import { mainnet } from 'viem/chains';
-import GLXRClaim from '../artifacts/contracts/Portal.json'; 
 import styles from '../components/modules/claim.module.css';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
 
-const tokenAddress = '0xA17051ebD6DF3b9Ad31fe6ad4fdE373b53DF1a6a'; 
-const claimAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138'; 
+const claimAddress = '0xBC779B536E4A18eA1E5A0796a3D757CD590fD69f';
 
-async function getMerkleProofs(userAddress: string): Promise<string[]> {
+async function getMerkleProofs(userAddress: string): Promise<{ balance: number; merkleProof: string[] }> {
   const response = await fetch(`https://yourserver.com/merkleProof/${userAddress}`);
   const data = await response.json();
-  return data.merkleProof;
+  return { balance: data.balance, merkleProof: data.merkleProof };
 }
 
 function Claim() {
-  const [balance, setBalance] = useState(null);
   const [account, setAccount] = useState<string | null>(null);
   const [walletClient, setWalletClient] = useState<any | null>(null);
   const [selectedDays, setSelectedDays] = useState(90);
-  const stakeDuration = selectedDays * 24 * 60 * 60; // convert days to seconds
   const [newBalance, setNewBalance] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const stakeDuration = selectedDays * 24 * 60 * 60; // convert days to seconds
 
   const handleStakeDuration = (days: number) => {
     setSelectedDays(days);
     let multiplier;
 
-    switch(days) {
+    switch (days) {
       case 180:
         multiplier = 10;
         break;
@@ -43,22 +40,30 @@ function Claim() {
         multiplier = 1;
     }
 
-    if (balance !== null) {
-      setNewBalance(balance * multiplier);
+    if (newBalance !== null) {
+      setNewBalance(newBalance * multiplier);
     }
-  }
+  };
 
   useEffect(() => {
     const fetchAccount = async () => {
-      // Create a Wallet Client using viem
       const client = createWalletClient({
         chain: mainnet,
-        transport: custom(window.ethereum)
+        transport: custom(window.ethereum),
       });
 
       const [address] = await client.getAddresses();
       setAccount(address);
       setWalletClient(client);
+
+      try {
+        if (address) {
+          const { balance } = await getMerkleProofs(address);
+          setNewBalance(balance);
+        }
+      } catch (err) {
+        setError('Failed to fetch balance from Merkle proof');
+      }
     };
     fetchAccount();
   }, []);
@@ -66,23 +71,21 @@ function Claim() {
   async function claimNewToken() {
     try {
       if (account === null) {
-        throw new Error("Account not loaded");
+        throw new Error('Account not loaded');
       }
-
-      const merkleProofs = await getMerkleProofs(account);
-
+  
+      const { merkleProof } = await getMerkleProofs(account);
+  
       const transactionResponse = await walletClient.sendTransaction({
         to: claimAddress,
-        data: walletClient.interface.encodeFunctionData('claimNewToken', [stakeDuration, balance, merkleProofs]),
+        data: walletClient.interface.encodeFunctionData('claimNewToken', [stakeDuration, newBalance, merkleProof]),
       });
-
+  
       console.log(`Transaction hash: ${transactionResponse.hash}`);
-    } catch (error) {
-      console.error(`Failed to claim tokens: ${error}`);
+    } catch (error: any) {
+      setError(`Failed to claim tokens: ${(error as Error).message}`);
     }
   }
-
-
   return (
     <>
       <Header/>
