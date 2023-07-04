ad2087d08cbba980b18ac6d07ba07b53fef39463
futjrnaut <futjrn@gmail.com>
2023-07-04 08:46:50 -0800

    fix map errors

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import Mapbox, { LngLatLike, Map as MapboxMap } from 'mapbox-gl';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../components/modules/map.module.css';
 
 import { useAccount, useSignTypedData } from 'wagmi';
-import MapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+import MapboxGeocoding, { GeocodeQueryType, GeocodeRequest } from '@mapbox/mapbox-sdk/services/geocoding';
 import { recoverTypedDataAddress } from 'viem';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FsYXhlciIsImEiOiJjbGkwN2x2NzgwN3ZkM2RwY2kyeXppeGkwIn0.-u0aVxVb8Nxyn-hvkyyZnw';
-const geocodingClient = MapboxGeocoding({ accessToken: mapboxgl.accessToken });
+Mapbox.accessToken = 'pk.eyJ1IjoiZ2FsYXhlciIsImEiOiJjbGkwN2x2NzgwN3ZkM2RwY2kyeXppeGkwIn0.-u0aVxVb8Nxyn-hvkyyZnw';
+const geocodingClient = MapboxGeocoding({ accessToken: Mapbox.accessToken });
 
 const domain = {
   name: 'Ether Mail',
   version: '1',
   chainId: 1,
   verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-} as const
+} as const;
 
 const types = {
   Person: [
@@ -27,33 +27,33 @@ const types = {
     { name: 'city', type: 'string' },
     { name: 'coordinates', type: 'string' },
   ],
-} as const
+} as const;
 
 export function Map() {
-  const mapContainerRef = useRef(null);
-  const mapRef = useRef(null);
-  const [location, setLocation] = useState(null);
-  const [city, setCity] = useState(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<MapboxMap | null>(null);
+  const [location, setLocation] = useState<number[] | null>(null);
+  const [city, setCity] = useState<string | null>(null);
   const account = useAccount();
-  const [recoveredAddress, setRecoveredAddress] = useState(null);
+  const [recoveredAddress, setRecoveredAddress] = useState<string | null>(null);
 
   const message = {
     name: 'Galaxer',
-    wallet: account?.address,
+    wallet: account?.address || '',
     city: city,
-    coordinates: location?.toString(),
-  } as const
+    coordinates: location ? location.toString() : undefined,
+  } as const;
 
   const { data, error, isLoading, signTypedData } = useSignTypedData({
     domain,
     types,
     message,
     primaryType: 'Person',
-  })
+  });
 
   useEffect(() => {
-    if (!data) return
-    ;(async () => {
+    if (!data) return;
+    (async () => {
       setRecoveredAddress(
         await recoverTypedDataAddress({
           domain,
@@ -62,61 +62,71 @@ export function Map() {
           primaryType: 'Person',
           signature: data,
         }),
-      )
-    })()
-  }, [data])
+      );
+    })();
+  }, [data]);
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40],
-      zoom: 9,
-    });
+    if (mapContainerRef.current) {
+      Mapbox.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+      const map = new Mapbox.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40],
+        zoom: 9,
+      });
 
-    mapRef.current = map;
+      mapRef.current = map;
 
-    return () => map.remove();
+      return () => map.remove();
+    }
   }, []);
 
   function hubMyLocation() {
     if (!navigator.geolocation) {
       console.log('Geolocation is not supported by your browser');
     } else {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const coords = [position.coords.longitude, position.coords.latitude];
-        mapRef.current.flyTo({
-          center: coords,
-          essential: true
-        });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const coords: LngLatLike = [position.coords.longitude, position.coords.latitude];
+          if (mapRef.current) {
+            mapRef.current.flyTo({
+              center: coords,
+              essential: true,
+            });
 
-        new mapboxgl.Marker()
-          .setLngLat(coords)
-          .addTo(mapRef.current);
+            new Mapbox.Marker().setLngLat(coords).addTo(mapRef.current);
 
-        setLocation(coords);
+            setLocation(coords);
 
-        geocodingClient.reverseGeocode({
-          query: coords,
-          limit: 1,
-          types: 'place'
-        })
-        .send()
-        .then(response => {
-          const match = response.body.features[0];
-          setCity(match.place_name);
-        });
+            const geocodeRequest: GeocodeRequest = {
+              query: coords,
+              limit: 1,
+              types: ['place'] as GeocodeQueryType[],
+            };
 
-      }, (error) => {
-        console.log('Unable to retrieve your location', error);
-      });
+            geocodingClient
+              .reverseGeocode(geocodeRequest)
+              .send()
+              .then((response) => {
+                const match = response.body.features[0];
+                setCity(match.place_name);
+              });
+          }
+        },
+        (error) => {
+          console.log('Unable to retrieve your location', error);
+        }
+      );
     }
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', width: '100%' }}>
-        <button onClick={hubMyLocation} style={{ position: 'absolute', top: '10px' }}>Find Me</button>
+        <button onClick={hubMyLocation} style={{ position: 'absolute', top: '10px' }}>
+          Find Me
+        </button>
         <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
       </div>
       {location && <p>Location: {location[0]}, {location[1]}</p>}
